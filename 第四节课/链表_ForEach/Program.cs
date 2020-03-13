76e27b9603aaf071a7600d01ac7393baa436cb1c
PeterSH6 <petershengwhu@gmail.com>
2020-03-13 21:59:06 +0800

    第四次课堂代码

---
+++ 第四节课/链表_ForEach/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication {
+
+  // 链表节点
+  public class Node<T> 
+  {
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t) {
+      Next = null;
+      Data = t;
+    }
+  }
+
+/* 
+1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。 */
+
+  //泛型链表类
+  public class GenericList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T> temp;
+        for(temp = head; temp != null ; temp = temp.Next)
+        {
+            action(temp.Data);
+        }
+    }
+
+    public GenericList() {
+      tail = head = null;
+    }
+
+    public Node<T> Head {
+      get => head;
+    }
+
+    public void Add(T t) 
+    {
+      Node<T> n = new Node<T>(t);
+      if (tail == null)
+      {
+        head = tail = n;
+      }
+      else 
+      {
+        tail.Next = n;
+        tail = n;
+      }
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      // 整型List
+       GenericList<int> intlist = new GenericList<int>();
+      for (int x = 0; x < 10; x++) {
+        intlist.Add(x);
+      }
+      
+      //逐个打印
+      intlist.ForEach(s => Console.WriteLine(s));
+      //求最大值
+      int max = int.MinValue;
+      intlist.ForEach( s => {if(s>max) max = s;});
+      Console.WriteLine("最大值为："+max);
+      //求最小值
+      int min = int.MaxValue;
+      intlist.ForEach(s => {if(s<min) min = s;});
+      Console.WriteLine("最小值为："+min);
+      //求和
+      int sum = 0;
+      intlist.ForEach( s => {sum += s ;});
+      Console.WriteLine("列表元素的和为："+sum);
+
+      /*
+      // 字符串型List
+      GenericList<string> strList = new GenericList<string>();
+      for (int x = 0; x < 10; x++) {
+        strList.Add("str"+x);
+      }
+      for (Node<string> node = strList.Head; 
+              node != null; node = node.Next) {
+        Console.WriteLine(node.Data);
+      } */
+
+
+
+    }
+
+  }
+}
---
+++ 第四节课/闹钟_事件委托/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+
+
+namespace 第四节课_2
+{
+    /* 2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+    在闹钟走时时或者响铃时，在控制台显示提示信息。 */
+    class Program
+    {
+        public delegate void ClockHandler(ClockEventArgs Args);
+
+        public class ClockEventArgs:EventArgs 
+        {
+            private DateTime currenttime;
+            public DateTime CurrentTime{get=>currenttime;set=>currenttime = value;}
+            private DateTime settime;
+            public DateTime SetTime{get=>settime;set=>settime = value;}
+            
+        }
+        public class ClockEvent
+        {
+            private bool flag;//用于记录是否将闹钟按停止
+            public event ClockHandler TravelTime;
+            public event ClockHandler Ring;
+
+            public void CallClock(DateTime clockring)
+            {   //TODO for
+                ClockEventArgs args = new ClockEventArgs();
+                args.SetTime = clockring;
+                while(true)//无限循环
+                {
+                    args.CurrentTime = System.DateTime.Now;
+                    if(DateTime.Compare(args.CurrentTime,args.SetTime) > 0 && !flag)
+                    //不可以用==0，很难严格相等，大于即开始闹钟
+                    {
+                        Console.WriteLine("该起床了！！！");
+                        if(Ring != null)
+                            Ring(args);
+                        if(Console.ReadLine() != null) flag = true;
+                        //当用户键入任意值(直接回车），停止闹钟，继续进入走时。
+                    }
+                    else 
+                    {
+                        Console.WriteLine("现在的时间是："+ DateTime.Now.ToString());
+                        if(TravelTime != null)
+                            TravelTime(args);
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public class User
+        {
+            static void Main()
+            {
+                var clockevent = new ClockEvent();
+                DateTime set = new DateTime();
+                int y,m,d,h,mi,s;
+                Console.WriteLine("请输入时间：年，月，日，小时，分钟，秒");
+                try{
+                    y = int.Parse(Console.ReadLine());
+                    m = int.Parse(Console.ReadLine());
+                    d = int.Parse(Console.ReadLine());
+                    h = int.Parse(Console.ReadLine());
+                    mi = int.Parse(Console.ReadLine());
+                    s = int.Parse(Console.ReadLine());
+                    set = new DateTime(y,m,d,h,mi,s);
+                    Console.WriteLine("闹钟时间是："+ set.ToString());
+                }
+                catch(ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("输入的时间超过范围");
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("输入格式有误");
+                }
+                finally
+                {
+                    clockevent.CallClock(set);//开启时钟,无论输入是否正确
+                }
+            }
+
+            /* static void show(ClockEventArgs args)
+            {
+            } */
+        }
+       
+    }
+}
