b2a7e857452835c33ed8cc7209fb5b6702d9e7b1
PeterSH6 <13302211862@189.cn>
2020-04-05 23:55:23 +0800

    第7节课CalleyTree

---
+++ CayleyTree/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿using System.Drawing;
+
+namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pnlCayleyTree = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(184, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnDrawClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "画笔颜色";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-4, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "递归深度（n）";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.trackBar6);
+            this.panel1.Controls.Add(this.trackBar5);
+            this.panel1.Controls.Add(this.trackBar4);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(785, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 822);
+            this.panel1.TabIndex = 14;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(180, 729);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(22, 24);
+            this.label13.TabIndex = 41;
+            this.label13.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(180, 630);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 24);
+            this.label12.TabIndex = 40;
+            this.label12.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(180, 547);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 24);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(180, 458);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 24);
+            this.label10.TabIndex = 38;
+            this.label10.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(180, 369);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(22, 24);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(180, 288);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 24);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "0";
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.LargeChange = 1;
+            this.trackBar6.Location = new System.Drawing.Point(224, 718);
+            this.trackBar6.Maximum = 90;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(162, 90);
+            this.trackBar6.TabIndex = 35;
+            this.trackBar6.Scroll += new System.EventHandler(this.trbTh2Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.LargeChange = 1;
+            this.trackBar5.Location = new System.Drawing.Point(224, 633);
+            this.trackBar5.Maximum = 90;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(162, 90);
+            this.trackBar5.TabIndex = 34;
+            this.trackBar5.Scroll += new System.EventHandler(this.trbTh1Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.LargeChange = 1;
+            this.trackBar4.Location = new System.Drawing.Point(224, 546);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(162, 90);
+            this.trackBar4.TabIndex = 33;
+            this.trackBar4.Scroll += new System.EventHandler(this.trbPer2Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(224, 457);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(162, 90);
+            this.trackBar3.SmallChange = 0;
+            this.trackBar3.TabIndex = 32;
+            this.trackBar3.Scroll += new System.EventHandler(this.trbPer1Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(224, 379);
+            this.trackBar2.Maximum = 150;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(162, 90);
+            this.trackBar2.TabIndex = 31;
+            this.trackBar2.Scroll += new System.EventHandler(this.trbLengthScroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(224, 279);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(162, 90);
+            this.trackBar1.TabIndex = 30;
+            this.trackBar1.Scroll += new System.EventHandler(this.trbRecrusiveScroll);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 40);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "选择颜色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnColorClick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(0, 718);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 26);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "左分支角度";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(0, 633);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 26);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "右分支角度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(0, 546);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 26);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "左分支长度比";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(0, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 26);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "右分支长度比";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(0, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 26);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "主干长度";
+            // 
+            // pnlCayleyTree
+            // 
+            this.pnlCayleyTree.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlCayleyTree.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlCayleyTree.Location = new System.Drawing.Point(0, 0);
+            this.pnlCayleyTree.Name = "pnlCayleyTree";
+            this.pnlCayleyTree.Size = new System.Drawing.Size(779, 814);
+            this.pnlCayleyTree.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 814);
+            this.Controls.Add(this.pnlCayleyTree);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel pnlCayleyTree;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CayleyTree/CayleyTree/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree
+{
+    //将课本中例5-31的Cayley树绘图代码进行修改。添加一组控件用以调节树的绘制参数。
+    //参数包括递归深度（n）、主干长度（leng）、右分支长度比（per1）、
+    //左分支长度比（per2）、右分支角度（th1）、左分支角度（th2）、画笔颜色（pen）。
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        Pen color = Pens.Black;
+        double x0;
+        double y0;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pnlCayleyTree.CreateGraphics();
+            x0 = pnlCayleyTree.Width / 2;
+            y0 = pnlCayleyTree.Height;
+        }
+
+        private void btnColorClick(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color = new Pen(colorDialog1.Color);
+            }
+        }
+        private void btnDrawClick(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            int n = trackBar1.Value;
+            int length = trackBar2.Value;
+            per1 = trackBar3.Value / 100d;
+            per2 = trackBar4.Value / 100d;
+            int th1_temp = trackBar5.Value;
+            int th2_temp = trackBar6.Value;
+            th1 = th1_temp* Math.PI / 180; 
+            th2 = th2_temp* Math.PI / 180;
+            graphics.Clear(pnlCayleyTree.BackColor);
+            drawCayleyTree(n, x0, y0, length,-Math.PI / 2);
+        }
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th-th2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(color, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void trbRecrusiveScroll(object sender, EventArgs e)
+        {
+            label8.Text = trackBar1.Value.ToString();
+        }
+
+        private void trbLengthScroll(object sender, EventArgs e)
+        {
+            label9.Text = trackBar2.Value.ToString();
+        }
+
+        private void trbPer1Scroll(object sender, EventArgs e)
+        {
+            label10.Text = (trackBar3.Value/100f).ToString();
+        }
+        
+        private void trbPer2Scroll(object sender, EventArgs e)
+        {
+            label11.Text = (trackBar4.Value/100f).ToString();
+        }
+
+        private void trbTh1Scroll(object sender, EventArgs e)
+        {
+            label12.Text = trackBar5.Value.ToString();
+        }
+
+        private void trbTh2Scroll(object sender, EventArgs e)
+        {
+            label13.Text = trackBar6.Value.ToString();
+        }
+    }
+}
